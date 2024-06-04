7a40b298675b6b32d8a75144f672132be920244c
robinlafontaine <robin@lafontaines.eu>
2024-06-04 08:56:47 +0200

    added send button

--- lib/widgets/prompt_input.dart
+++ lib/widgets/prompt_input.dart
@@ -15,25 +15,29 @@ class _PromptInputState extends State<PromptInput> {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      textInputAction: TextInputAction.newline,
       autofocus: false,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(50),
           borderSide: BorderSide.none,
         ),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: () {
+            widget.onSubmitted(_controller.text);
+            _controller.clear();
+          },
+        ),
         contentPadding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
         filled: true,
-        fillColor: Theme.of(context).colorScheme.surfaceVariant,
+        fillColor: Theme.of(context).colorScheme.surfaceContainerHighest,
         hintStyle: TextStyle(
           color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
         ),
         hintText: 'Ma question...',
       ),
       controller: _controller,
-      onSubmitted: (String value) {
-        widget.onSubmitted(value);
-        _controller.clear();
-      },
     );
   }
 }
