df162be51fcc1974a3f76b838e04b6f15d2890c4
Miunn <remcaulier@orange.fr>
2024-06-07 16:28:27 +0200

    Circular progress indicator on loading suggestions

--- lib/screens/suggestions.dart
+++ lib/screens/suggestions.dart
@@ -14,6 +14,7 @@ class SuggestionsScreen extends StatefulWidget {
 }
 
 class _SuggestionsScreenState extends State<SuggestionsScreen> {
+  bool loading = false;
   List<Suggestion> suggestions = [];
   final DateFormat formatter = DateFormat('dd/MM/yyyy HH:mm');
 
@@ -24,10 +25,11 @@ class _SuggestionsScreenState extends State<SuggestionsScreen> {
   }
 
   loadSuggestions() async {
+    loading = true;
     List<Suggestion> suggestions =
         await SuggestionsAPIInterface.getSuggestions();
     suggestions.sort((a, b) => b.createdAt!.compareTo(a.createdAt!));
-
+    loading = false;
     setState(() {
       this.suggestions = suggestions;
     });
@@ -46,101 +48,107 @@ class _SuggestionsScreenState extends State<SuggestionsScreen> {
         },
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: ListView.builder(
-            itemCount: suggestions.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card.filled(
-                child: ListTile(
-                  title: Text(suggestions[index].message ?? ""),
-                  subtitle: Row(
-                    children: [
-                      Text(suggestions[index].user ?? ""),
-                      const Spacer(),
-                      Text(formatter.format(suggestions[index].createdAt ?? DateTime.now())),
-                    ],
-                  ),
-                ),
-              );
-            },
-            physics: const AlwaysScrollableScrollPhysics(),
-          ),
+          child: (loading)
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                      itemCount: suggestions.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card.filled(
+                          child: ListTile(
+                            title: Text(suggestions[index].message ?? ""),
+                            subtitle: Row(
+                              children: [
+                                Text(suggestions[index].user ?? ""),
+                                const Spacer(),
+                                Text(formatter.format(
+                                    suggestions[index].createdAt ??
+                                        DateTime.now())),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      physics: const AlwaysScrollableScrollPhysics(),
+                    ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showDialog(context: context, builder: (BuildContext context) {
-            GlobalKey<FormState> formKey = GlobalKey<FormState>();
-            Suggestion suggestion = Suggestion(
-              message: "",
-              user: "",
-              createdAt: DateTime.now(),
-            );
-            return AlertDialog(
-              title: const Text("Ajouter une suggestion"),
-              content: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      minLines: 1,
-                      maxLines: 5,
-                      keyboardType: TextInputType.multiline,
-                      textInputAction: TextInputAction.newline,
-                      decoration: const InputDecoration(
-                        hintText: "Votre suggestion",
-                      ),
-                      onChanged: (String value) {
-                        suggestion.message = value;
-                      },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "Veuillez entrer une suggestion";
-                        }
-                        return null;
-                      },
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                GlobalKey<FormState> formKey = GlobalKey<FormState>();
+                Suggestion suggestion = Suggestion(
+                  message: "",
+                  user: "",
+                  createdAt: DateTime.now(),
+                );
+                return AlertDialog(
+                  title: const Text("Ajouter une suggestion"),
+                  content: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          minLines: 1,
+                          maxLines: 5,
+                          keyboardType: TextInputType.multiline,
+                          textInputAction: TextInputAction.newline,
+                          decoration: const InputDecoration(
+                            hintText: "Votre suggestion",
+                          ),
+                          onChanged: (String value) {
+                            suggestion.message = value;
+                          },
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Veuillez entrer une suggestion";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: "Votre nom",
+                          ),
+                          onChanged: (String value) {
+                            suggestion.user = value;
+                          },
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Veuillez entrer votre nom";
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: "Votre nom",
-                      ),
-                      onChanged: (String value) {
-                        suggestion.user = value;
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
                       },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "Veuillez entrer votre nom";
+                      child: const Text("Annuler"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        if (formKey.currentState!.validate() == false) {
+                          return;
                         }
-                        return null;
+
+                        await SuggestionsAPIInterface.addSuggestion(suggestion);
+                        loadSuggestions();
+                        if (context.mounted) {
+                          Navigator.of(context).pop();
+                        }
                       },
+                      child: const Text("Envoyer"),
                     ),
                   ],
-                ),
-              ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("Annuler"),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    if (formKey.currentState!.validate() == false) {
-                      return;
-                    }
-
-                    await SuggestionsAPIInterface.addSuggestion(suggestion);
-                    loadSuggestions();
-                    if (context.mounted) {
-                      Navigator.of(context).pop();
-                    }
-                  },
-                  child: const Text("Envoyer"),
-                ),
-              ],
-            );
-          });
+                );
+              });
         },
         child: const Icon(Icons.send),
       ),
