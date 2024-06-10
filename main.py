51d5b3b21f154af6f70a98ec7c4d17ecf1bbbed8
Robin Lafontaine <46829268+robinlafontaine@users.noreply.github.com>
2024-06-10 12:39:48 +0200

    added vector db

--- main.py
+++ main.py
@@ -1,40 +1,99 @@
 import json
-import re
-from flask import Flask
-from flask import jsonify
-from flask import request
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from sys import argv
+from flask import Flask, send_file, jsonify, request
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
+forget = 0
+context_depth = 4
 model_id = "google/gemma-1.1-2b-it"
-#model_id = "microsoft/Phi-3-mini-128k-instruct"
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id,
     torch_dtype="auto",
-    trust_remote_code=True,
+    attn_implementation="sdpa"
 )
 
 tokenizer.chat_template = "{{ bos_token }}{%for message in messages %}{% if (message['role'] == 'assistant') %}{% set role = 'model' %}{% else %}{% set role = message['role'] %}{% endif %}{{ '<start_of_turn>' + role + ' ' + message['content'] | trim + '<end_of_turn>' }}{% endfor %}{% if add_generation_prompt %}{{'<start_of_turn>model'}}{% endif %}"
 
 model.to(device)
 
+def init_context(json_file_path:str) -> Chroma:
 
-def askGPT(message:list):
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    texts = []
+    for entry in data:
+        text = entry.get('text', '')
+        documents_content = entry.get('documents', '')
+        if isinstance(documents_content, dict):
+            documents_content = json.dumps(documents_content)
+        combined_text = text + ' ' + documents_content
+        texts.append(combined_text)
+
+    chunks = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, separators=["\n\n", "\n", " ", ""])
+    for text in texts:
+        chunks.extend(text_splitter.create_documents([text]))
+
+    embeddings_model = HuggingFaceBgeEmbeddings(
+        model_name='sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2',
+        model_kwargs={'device': device},
+        encode_kwargs={'normalize_embeddings': True},
+        query_instruction=""
+    )
+
+    chunk_texts = [chunk.page_content for chunk in chunks]
+
+    embeddings = embeddings_model.embed_documents(chunk_texts)
+
+    print(f'{len(embeddings)} documents loaded')
+
+    return Chroma.from_documents(chunks, embeddings_model)
+
+
+def best_context(prompt:str, vectorstore:Chroma) -> list:
+
+    docs = vectorstore.similarity_search_with_relevance_scores(prompt, k=context_depth)
+    print(f'{len(docs)} documents found')
+
+    #TODO : threshold for relevance score
+    docs_filtered = [doc for doc in docs if doc[1] < 0.3]
+    docs_texts = [doc[0].page_content for doc in docs_filtered]
+
+    context = [{"role": "system", "content": text} for text in docs_texts]
+        
+    return context
+
+def askGPT(message:list, context:list) -> str:
+
     global model
     global tokenizer
+    global forget
 
-    messages = [
-        {"role": "system", "content": "Vous êtes le chatbot de l'UPHF, votre rôle est d'assister et de répondre aux questions des utilisateurs."},
-        {"role": "assistant", "content": "Ok, je retiens que je suis un chatbot de l'UPHF et que mon rôle est d'assister et de répondre aux questions des utilisateurs."},
-    ]
+    messages = []
+    directive = {"role": "system", "content": "A partir de maintenant vous êtes un assistant de l'UPHF (l'Université Polytechnique des Hauts-de-France) pour les tâches de questions-réponses. Utilisez les éléments de contexte récupérés suivants pour répondre à la question. Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas. Utilisez un maximum de trois phrases et gardez la réponse concise"}
 
-    if not json.loads(message):
+    client_msg = json.loads(message)
+
+    if not client_msg:
         return "Veuillez poser une question..."
 
-    messages = messages + json.loads(message) 
+    if not context:
+        return "Je n'ai pas trouvé de réponse à votre question..."
+
+    messages.extend(context)
+    messages.append(directive)
+
+    if forget > 0 and len(client_msg) > forget:
+        messages.extend(client_msg[:-forget])
+    else:
+        messages.extend(client_msg)
 
     input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
 
@@ -42,16 +101,15 @@ def askGPT(message:list):
 
     gen_tokens = model.generate(
         input_ids,
-        max_new_tokens=300, 
-        do_sample=True, 
+        max_new_tokens=300,
+        do_sample=True,
         temperature=0.3,
     )
 
     gen_text = tokenizer.decode(gen_tokens[0])
 
     gen_text = gen_text.split("<start_of_turn>")[-1].strip()
-    pattern = r"model (.*?)<eos>"
-    gen_text = re.search(pattern, gen_text).group(1)
+    gen_text = gen_text[5:-5]
 
     return gen_text
 
@@ -68,9 +126,35 @@ def getAnswer():
     if not content:
         return jsonify({'role': 'assistant', 'content': 'Veuillez poser une question...'})
     print("Handling request with prompt: " + content)
-    answer = askGPT(content)
+    context = best_context("Qu'est-ce l'UPHF ?", vectorstore)
+    answer = askGPT(content, context)
     return jsonify({'role': 'assistant', 'content': answer})
 
+@app.route('/download', methods=['GET'])
+def getFiles():
+    os_type = request.args.get('platform', '')
+    if os_type == 'ios':
+        return send_file('C:/Users/robin/Desktop/Projet S8/downloads/image.png')
+    elif os_type == 'android':
+        return send_file('C:/Users/robin/Desktop/Projet S8/downloads/image.png')
+    else:
+        return "Error: Invalid OS type", 404
+
 if __name__ == "__main__":
-    print(str(model.get_memory_footprint()*10**-9) + ' GB')
+
+    if len(argv) > 1:
+        if argv[1].startswith('--forget='):
+            forget = int(argv[1].split('=')[1])
+        else:
+            print("Usage: python main.py [--forget=<int>]")
+            exit(1)
+    
+    print(f'Model Memory : {model.get_memory_footprint()*10**-9} GB')
+    
+    vectorstore = init_context('C:\\Users\\robin\\Desktop\\Projet S8\\page_content_test.json')
+
+    #TODO: Clean data and make sure chunk sizes are around 1000 (keep single newlines for paragraph splits)
+    #NOTE: Remove single newline replacements in json_parse
+    #TODO: Chunk sizes are throttling the model memory, need to find a way to reduce memory usage
+
     app.run(host='127.0.0.1', port=34197)
